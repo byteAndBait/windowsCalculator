fb377eae1032b2a5e2e5393839fdcb5a9549b264
byteAndBait <192332038+byteAndBait@users.noreply.github.com>
2025-05-17 09:36:10 +0300

    Add the main structure  - full html structure  - few css  - plan for js logic

---
+++ main.js
@@ -0,0 +1,23 @@
+
+/*
+    we want a calculator that can do +,-,/,* on each pair of numbers.
+
+    given a display element and textContent from a delegation event we should operate.
+
+    make a variables Total,firstNumber, secondNumber and the input.
+
+    make references with delegation for numbers and operators,special functions.
+
+    make a function checkInput that when the one of the operators is clicked, if the input
+    has a value so put the value to the firstNumber varaible , and when it's clicked again 
+    check whether the firstNumber has value if so put it to the secondNumber .
+
+    and in every click check if the both numbers have values if so operate(first,second,op).
+    in operate() make condition for every operation and then assign the result to the total
+    and assign the firstNumber value to the total value so we can use it again, and display the total.
+
+    when = is clicked do operate.
+    when AC is clicked reset all the values and display zero.
+    
+    **Note** when clicking a number make the firstNumber = 0
+    */
